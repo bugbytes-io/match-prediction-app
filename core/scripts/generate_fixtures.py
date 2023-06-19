422fd2313236e03710be3ab2080c9aa5a8212d9e
bugbytes-io <bugbytes.io@gmail.com>
2023-06-19 15:28:55 +0100

    Initial

---
+++ core/scripts/generate_fixtures.py
@@ -0,0 +1,22 @@
+# from core.models import Fixture, Team
+from django.utils import timezone
+
+# Register your models here.
+def run():
+    teams = [
+        "Arsenal",
+        "Chelsea",
+        "Liverpool",
+        "Manchester City",
+        "Manchester United",
+        "Tottenham Hotspur",
+        "Real Madrid",
+        "Barcelona",
+        "Juventus",
+        "Inter Milan",
+        "Bayern Munich",
+        "Ajax",
+        "PSG",
+        "Porto"
+    ]
+
